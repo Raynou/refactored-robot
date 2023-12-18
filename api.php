45598b5c5f2226c4c31e236604cb6c5fb12041c6
Raymonds <rafaelgame2012@gmail.com>
2023-12-17 20:25:25 -0600

    Add API for connect with AWS Rekognittion
    - A new file called api.php was created. It's for connect the camera
      module with the Apache Server and AWS
    - Camera module was updated for sends the photo taken, user id, hour and page
      title where the photo has beed taken.
    - Stormwind was added.
    - Now get_content() function from block_simplecamera.php gets the loged
      user id and the page's title where the block is rendered and sends
      them to the camera module.
    - Add Spanish doc.
    - Add English doc.

---
+++ api.php
@@ -0,0 +1,36 @@
+<?php
+require_once("./vendor/autoload.php");
+
+header("Conten-Type:application/json");
+$method = $_SERVER["REQUEST_METHOD"];
+
+use Stormwind\FaceAnalyzer;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+switch($method) {
+    case "POST":
+        $requestBody = file_get_contents('php://input');
+
+        $logs = fopen("logs.txt", "wb");
+
+        $requestBody  = ((array) json_decode($requestBody));
+        
+        $photoURI = $requestBody['photo'];
+        
+        $feelings = FaceAnalyzer::detectFeelings(explode(",", $photoURI)[1]);
+        
+        $photoInfo = $requestBody['page'] . "\n" . $requestBody['userId'] . "\n" . $requestBody['time'] . "\n" . print_r($feelings, true);
+        fwrite($logs, $photoInfo);
+
+        echo(json_encode($feelings));
+
+        break;
+    case "GET":
+        $requestBody = file_get_contents('php://input');
+        $logs = fopen("logs", "wb");
+        fwrite($log, "You've send a GET");
+        break;
+}
