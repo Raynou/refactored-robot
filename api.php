6f965aa88989b96054f6e15bbd3ec5517c827420
Raymonds <rafaelgame2012@gmail.com>
2024-01-10 09:34:35 -0600

    add logic in api.php for store feelings analysis in moodle database

--- api.php
+++ api.php
@@ -5,15 +5,19 @@ header("Conten-Type:application/json");
 $method = $_SERVER["REQUEST_METHOD"];
 
 use Stormwind\FaceAnalyzer;
+use Stormwind\QueryHandler;
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-function insert_record($database, $record) {
-    require_once(__DIR__ . '/../../config.php');
-    global $DB;
-    $DB->insert_record($database, $record);
+function insertRecord($record) {
+    try {
+        $handler = new QueryHandler();
+        $handler->insertAnalysis($record);
+    }catch(Exception $e) {
+        error_log("ERROR: " . $e);
+    }
 }
 
 switch($method) {
@@ -21,24 +25,20 @@ switch($method) {
         $requestBody = file_get_contents('php://input');
         
         $logs = fopen("logs.txt", "wb");
-        
         $requestBody  = ((array) json_decode($requestBody));
-        $record_details = new stdClass();
-        $record_details->user_id = $requestBody['userId'];
-        $record_details->timestamp = $requestBody['time'];
-        $record_details->page = $requestBody['page'];
         
         $photoURI = $requestBody['photo'];
-        
-        //$feelings = FaceAnalyzer::detectFeelings(explode(",", $photoURI)[1]);
-        
-        $photoInfo = $requestBody['page'] . "\n" . $requestBody['userId'] . "\n" . $requestBody['time'] . "\n" . print_r("", true) . "\n" . print_r($record, true);
-        
-        insert_record("block_simplecamera_details", $record_details);
-        
+        $feelings = FaceAnalyzer::detectFeelings(explode(",", $photoURI)[1]); 
+
+        $photoInfo = $requestBody['page'] . "\n" . $requestBody['userId'] . "\n" . $requestBody['time'] . "\n" . print_r("", true) . "\n" . print_r($feelings, true);
         fwrite($logs, $photoInfo);
 
-        //echo(json_encode($feelings));
+        $recordDetails = new stdClass();
+        $recordDetails->user_id = $requestBody['userId'];
+        $recordDetails->timestamp = $requestBody['time'];
+        $recordDetails->page = $requestBody['page'];
+        $recordDetails->sentiment = $feelings[0]["Type"];
+        insertRecord($recordDetails);
 
         break;
     case "GET":
