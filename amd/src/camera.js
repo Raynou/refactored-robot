6fc16812c987b52aa286d571a3a5841105a2509b
Raynou <rafaelgame2012@gmail.com>
2024-11-10 20:09:00 -0600

    lil refactor to js code and use relative url in fetch

--- amd/src/camera.js
+++ amd/src/camera.js
@@ -1,84 +1,80 @@
 /**
- * Saves the image and ...
+ * Saves the image
  * @param {video} video - The camera feedback
  * @param {canvas} canvas - The canvas where the image will be temporally saved
  * @param {string} title The title of the page where the photo is taken
  * @param {number} userId The id of the user
- * @param {string} url The API's URL
- * @returns Nothing
  */
-function save_image(video, canvas, title, userId, url) {
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+const saveImage = (video, canvas, title, userId) => {
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
     const date = new Date();
-
     const formatMinutes = (minutes) => {
         const stringMinutes = minutes.toString();
-        if(stringMinutes.length == 1) {
+        if (stringMinutes.length === 1) {
             return "0" + stringMinutes;
         }
         return stringMinutes;
     };
 
     const time = `${date.getHours()}:${formatMinutes(date.getMinutes())}`;
-    const mockBody = {
+    const body = {
         photo: canvas.toDataURL(),
-        page:  title,
+        page: title,
         userId: userId,
-        time: time
+        time: time,
     };
 
-    console.log(mockBody);
-
-    fetch(url, {
+    fetch("/moodle/blocks/simplecamera/api.php", {
         method: "POST",
         mode: "no-cors",
         headers: {
-            "Content-type":"application/json"
+            "Content-type": "application/json",
         },
-        body: JSON.stringify(mockBody)
+        body: JSON.stringify(body),
     });
-}
+};
 
 /**
  * Get the current time value from local storage
  * @returns {number} A number that represents time.
  */
-function getFromLocalStorage() {
+const getFromLocalStorage = () => {
     return parseInt(localStorage.getItem("timeTranscurred"));
-}
+};
 
 /**
  * Put a value of the local storage.
  * @param {number} seconds A number which represents the time transcurred in the timer
  */
-function pushToLocalStorage(seconds) {
+const pushToLocalStorage = (seconds) => {
     localStorage.setItem("timeTranscurred", seconds.toString());
-}
+};
 
-export const init = (timer, title, id, url) => {
+export const init = (timer, title, id) => {
     let video = document.querySelector("#camera");
     let canvas = document.querySelector("#canvas");
-    let save_button = document.querySelector("#save_button");
+    let saveButton = document.querySelector("#save_button");
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then((stream) => {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch((error) => window.alert(error));
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then((stream) => {
-        video.srcObject = stream;
-        video.play();
-    }).catch((error) => window.alert(error));
-
-    save_button.addEventListener('click', function() {
-        save_image(video, canvas);
+    saveButton.addEventListener("click", () => {
+        saveImage(video, canvas);
     });
 
-    if(getFromLocalStorage() === null || isNaN(getFromLocalStorage())) {
+    if (getFromLocalStorage() === null || isNaN(getFromLocalStorage())) {
         pushToLocalStorage(0);
     }
+
     setInterval(() => {
-        if(getFromLocalStorage()  === timer/1000) {
-            save_image(video, canvas, title, id, url);
+        if (getFromLocalStorage() === timer / 1000) {
+            saveImage(video, canvas, title, id);
             pushToLocalStorage(0);
-        }else {
-            const aux = getFromLocalStorage();
-            pushToLocalStorage(aux+1);
+        } else {
+            pushToLocalStorage(getFromLocalStorage() + 1);
         }
     }, 1000);
 };
